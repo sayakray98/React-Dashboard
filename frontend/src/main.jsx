b76bad21edb81477afcb04738f9b6853ab243774
Deepak Kumar <deepak@example.com>
2026-02-06 19:14:44 +0530

    Initial commit: frontend and backend setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
 );

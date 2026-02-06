e98b3c45aa1a0bcfe3727b644b793d78d20a2f45
Deepak Kumar <deepak@example.com>
2026-02-06 20:02:49 +0530

    Ensure all backend dependencies installed

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,13 @@
-const mongoose = require('mongoose');
-const mongooseURI = 'mongodb://localhost:27017/';
+const mongoose = require("mongoose");
 
-const connectToMongoose = () => {
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
 
-    mongoose.connect(mongooseURI);
-    console.log('Database Connection Successfully!')
-
-}
-
-module.exports = connectToMongoose;
+module.exports = connectDB;

8b576e401d5f2bec6843a9b4a9c744c41f44717c
Deepak Kumar <deepak@example.com>
2026-02-08 00:42:39 +0530

    Ensure all backend dependencies installed

--- backend/passport.js
+++ backend/passport.js
@@ -10,7 +10,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/google/callback",
+      callbackURL: "https://react-dashboard-5odm.onrender.com/api/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
